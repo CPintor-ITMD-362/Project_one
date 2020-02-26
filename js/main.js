d2ca641b11ccdc3473afa69bf52859c1b3c1f0cc
Cristian Pintor <cpintor@hawk.iit.edu>
2020-02-25 19:46:42 -0600

    Fixing JS messages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-function messageFunction() {
-  document.getElementById("output").innerHTML = "Congrats, you've registered!";
+function validate() {
+  var x = document.forms["registration"]["user_name"].value;
+  if (x == "" || x == null) {
+    alert("Plese fill out the form!");
+    return false;
+  } else {
+    document.getElementById("output").innerHTML = "Congrats, you've registered!";
+  }
 }
